5bee192b2934a8184f9846eabc7d48a2f677c8dd
Mohamed Ebrahim <ebrahimm131@gmail.com>
2025-12-22 16:59:06 +0400

    add WeatherApplication to configure Dagger hilt and timber for logging

---
+++ app/src/main/kotlin/com/dvt/weatherapp/WeatherApplication.kt
@@ -0,0 +1,14 @@
+package com.dvt.weatherapp
+
+import android.app.Application
+
+
+class WeatherApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
