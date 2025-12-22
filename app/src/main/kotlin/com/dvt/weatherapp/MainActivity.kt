04e30d3307006e94c328783e2019291967670a4f
Mohamed Ebrahim <ebrahimm131@gmail.com>
2025-12-22 17:31:59 +0400

    enable edge to edge screen for android 16

--- app/src/main/kotlin/com/dvt/weatherapp/MainActivity.kt
+++ app/src/main/kotlin/com/dvt/weatherapp/MainActivity.kt
@@ -2,9 +2,14 @@ package com.dvt.weatherapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.DisposableEffect
 
 class MainActivity : ComponentActivity() {
 
@@ -15,4 +20,6 @@ class MainActivity : ComponentActivity() {
         splashScreen.setKeepOnScreenCondition {
             false
         }
-}
+        enableEdgeToEdge()
+
+    }
