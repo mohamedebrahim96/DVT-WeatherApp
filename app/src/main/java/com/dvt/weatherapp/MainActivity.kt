7fa1f113f343d7555a786bedfef949e2fe2cc9b7
Mohamed Ebrahim <ebrahimm131@gmail.com>
2025-12-22 16:37:36 +0400

    add MainActivity with hello world text

---
+++ app/src/main/java/com/dvt/weatherapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.dvt.weatherapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HelloWorld()
+        }
+    }
+}
+
+@Composable
+fun HelloWorld() {
+    Text(text = "Hello World")
+}
