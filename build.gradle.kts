ed7c4c869714d4e6060ce748264416e3b31b5734
Mohamed Ebrahim <ebrahimm131@gmail.com>
2025-12-22 17:42:17 +0400

    add core module and domain module

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,4 +18,5 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.roborazzi) apply false
     alias(libs.plugins.secrets) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
